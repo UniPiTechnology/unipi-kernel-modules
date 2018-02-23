902dec7127790d9467ffdd2af337680830480caf
Tom Knot <tomasknot@gmail.com>
2018-02-23 16:42:13 +0100

    Updated Makefile

---
+++ modules/neuron_spi/src/unipi_uart.h
@@ -0,0 +1,22 @@
+/*
+ * UniPi Neuron tty serial driver - Copyright (C) 2018 UniPi Technologies
+ * Author: Tomas Knot <tomasknot@gmail.com>
+ *
+ *  Based on the SC16IS7xx driver by Jon Ringle <jringle@gridpoint.com>,
+ *  which was in turn based on max310x.c, by Alexander Shiyan <shc_work@mail.ru>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
+#ifndef MODULES_NEURON_SPI_SRC_UNIPI_UART_H_
+#define MODULES_NEURON_SPI_SRC_UNIPI_UART_H_
+
+
+
+
+
+#endif /* MODULES_NEURON_SPI_SRC_UNIPI_UART_H_ */
