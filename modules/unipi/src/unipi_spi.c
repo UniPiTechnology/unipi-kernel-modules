3db7517c6acd72710ee05af3dd51ad3a0e5545c3
Tom Knot <tomasknot@gmail.com>
2018-07-19 16:06:20 +0200

    Further renaming to the new naming scheme

--- modules/unipi/src/unipi_spi.c
+++ modules/unipi/src/unipi_spi.c
@@ -1,6 +1,5 @@
 /*
- * UniPi Neuron tty serial driver - Copyright (C) 2017 UniPi Tech
-nologies
+ * UniPi PLC device driver - Copyright (C) 2018 UniPi Technology
  * Author: Tomas Knot <tomasknot@gmail.com>
  *
  *  Based on the SC16IS7xx driver by Jon Ringle <jringle@gridpoint.com>,
