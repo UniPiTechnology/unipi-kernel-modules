144d67562cf717590b98340c55926ad82d2a1edc
Miroslav Ondra <ondra@faster.cz>
2019-09-19 22:46:30 +0200

    Change version string

--- modules/unipi/src/unipi_common.h
+++ modules/unipi/src/unipi_common.h
@@ -52,7 +52,7 @@
 #if NEURONSPI_SCHED_REQUIRED > 0
 	#include <uapi/linux/sched/types.h>
 #endif
-#define NEURONSPI_MAJOR_VERSIONSTRING "Version 1.28:2019:08:22"
+#define NEURONSPI_MAJOR_VERSIONSTRING "Version 1.32:2019:09:19"
 
 #define NEURONSPI_MAX_DEVS				3
 #define NEURONSPI_MAX_UART				16
