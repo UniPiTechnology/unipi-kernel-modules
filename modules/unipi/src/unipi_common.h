80e95a08de9c3a56a19dd526bed3bd576b7f314c
Martin Kudlacek <kudlacek@unipi.technology>
2021-02-08 14:48:17 +0100

    Bumped version to 1.62

--- modules/unipi/src/unipi_common.h
+++ modules/unipi/src/unipi_common.h
@@ -52,7 +52,7 @@
 #if NEURONSPI_SCHED_REQUIRED > 0
 	#include <uapi/linux/sched/types.h>
 #endif
-#define NEURONSPI_MAJOR_VERSIONSTRING "Version 1.61:2021:02:06"
+#define NEURONSPI_MAJOR_VERSIONSTRING "Version 1.62:2021:02:08"
 
 #define NEURONSPI_MAX_DEVS				3
 #define NEURONSPI_MAX_UART				16
