cfe11bc4e212d01ca6acb17b0628a94293a968b3
Miroslav Ondra <ondra@faster.cz>
2021-11-24 13:58:58 +0100

    Bump version to 1.80

--- modules/unipi/src/unipi_common.h
+++ modules/unipi/src/unipi_common.h
@@ -52,7 +52,7 @@
 #if NEURONSPI_SCHED_REQUIRED > 0
 	#include <uapi/linux/sched/types.h>
 #endif
-#define NEURONSPI_MAJOR_VERSIONSTRING "Version 1.78:2021:10:20"
+#define NEURONSPI_MAJOR_VERSIONSTRING "Version 1.80:2021:11:24"
 
 #define NEURONSPI_MAX_DEVS				7	//Maximal SPI card count
 #define NEURONSPI_MAX_UART				16
