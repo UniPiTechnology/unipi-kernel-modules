86c090a2e690fef6125c9e1369c38cb4dfb5730b
Miroslav Ondra <ondra@faster.cz>
2022-01-22 12:00:33 +0100

    Bump version to 1.82

--- modules/unipi/src/unipi_common.h
+++ modules/unipi/src/unipi_common.h
@@ -52,7 +52,7 @@
 #if NEURONSPI_SCHED_REQUIRED > 0
 	#include <uapi/linux/sched/types.h>
 #endif
-#define NEURONSPI_MAJOR_VERSIONSTRING "Version 1.80:2021:11:24"
+#define NEURONSPI_MAJOR_VERSIONSTRING "Version 1.82:2022:01:22"
 
 #define NEURONSPI_MAX_DEVS				7	//Maximal SPI card count
 #define NEURONSPI_MAX_UART				16
