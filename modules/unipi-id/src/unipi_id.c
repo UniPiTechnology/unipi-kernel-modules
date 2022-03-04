28b4b922f324a8054ec8080f3c77f2e1467324de
Miroslav Ondra <ondra@faster.cz>
2022-03-04 11:42:33 +0100

    WIP

--- modules/unipi-id/src/unipi_id.c
+++ modules/unipi-id/src/unipi_id.c
@@ -627,8 +627,8 @@ static struct i2c_client* unipi_id_load_client(struct i2c_adapter *adapter, unsi
 		return ERR_PTR(err);
 	
 	strncpy(info.type, eprom_type, I2C_NAME_SIZE);
-	info.properties = label_props_arr[index];
-	//dev_info(dev, "TRY %s,  %d %d", info.type,info.addr, index);
+	//info.properties = label_props_arr[index];
+	dev_info(dev, "TRY %s,  %d %d", info.type, info.addr, index);
 	return i2c_new_client_device(adapter, &info);
 }
 
