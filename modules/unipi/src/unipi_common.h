cdcaddb17931bdf610de85e123cad59c2e2ee9d7
Miroslav Ondra <ondra@faster.cz>
2022-04-19 18:37:35 +0200

    Bump version string 1.110

--- modules/unipi/src/unipi_common.h
+++ modules/unipi/src/unipi_common.h
@@ -52,7 +52,7 @@
 #if NEURONSPI_SCHED_REQUIRED > 0
 	#include <uapi/linux/sched/types.h>
 #endif
-#define NEURONSPI_MAJOR_VERSIONSTRING "Version 1.98:2022:03:11"
+#define NEURONSPI_MAJOR_VERSIONSTRING "Version 1.110:2022:04:19"
 
 #define NEURONSPI_MAX_DEVS				7	//Maximal SPI card count
 #define NEURONSPI_MAX_UART				16
